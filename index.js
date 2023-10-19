e8a92e57a3e6c121bef4664bb39e102260c3cce2
sam98963 <szwilliamson01@gmail.com>
2023-10-19 18:11:14 +0100

    starting constructure for Sprite class

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ canvas.width = 1024;
 canvas.height = 576;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor(position){
+    this.position = position;
+  }
+
+  draw(){
+    canvasContext.fillStyle = "red"
+    canvasContext.fillRect(this.position.x, this.position.y, 60, 150);
+  }
+}
+
+const player = new Sprite({
+  x:0,
+  y:0
+})
+
+const enemy = new Sprite({
+  x:400,
+  y:0
+})
+
+player.draw();
+enemy.draw();
