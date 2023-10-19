9f1d5dc41a065c97a240be4068949021dab916d9
sam98963 <szwilliamson01@gmail.com>
2023-10-19 18:21:53 +0100

    player movement and stay in screen

--- index.js
+++ index.js
@@ -6,26 +6,58 @@ canvas.height = 576;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
+
 class Sprite {
-  constructor(position){
+  constructor({position, velocity}){
     this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
   }
 
   draw(){
     canvasContext.fillStyle = "red"
-    canvasContext.fillRect(this.position.x, this.position.y, 60, 150);
+    canvasContext.fillRect(this.position.x, this.position.y, 60, this.height);
+  }
+
+  update(){
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if(this.position.y + this.height + this.velocity.y >= canvas.height){
+      this.velocity.y = 0;
+    }
   }
 }
 
 const player = new Sprite({
+  position:{
   x:0,
   y:0
-})
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  }})
 
 const enemy = new Sprite({
-  x:400,
-  y:0
-})
+  position:{
+    x:400,
+    y:0
+    },
+    velocity: {
+      x: 0,
+      y: 0,
+    }})
 
 player.draw();
 enemy.draw();
+
+function animate(){
+  window.requestAnimationFrame(animate)
+  canvasContext.fillStyle = "black"
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+}
+
+animate();
